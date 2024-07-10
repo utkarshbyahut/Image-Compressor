434c7b9da057364eb49f487c6919c840e95534ea
utkarshbyahut <utkarshbyahut16@gmail.com>
2024-07-10 14:28:56 -0700

    The module works without throwing errors for args

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 from PIL import Image
 
-def compress_image(input_path, output_path, quality):
-    """
+def compress_image(input_path, output_path, output_quality):
+    '''
     Compresses an image and saves it to the output path.
 
     Args:
         input_path: The path to the input image.
         output_path: The path to save the compressed image.
         quality: The compression quality (0-100).
-    """
+    '''
 
     img = Image.open(input_path)
-    img.save(output_path, optimize=True, quality=quality)
+    img.save(output_path, optimize = True, output_quality = output_quality)
 
 inputPath = input("Enter Local Path to the Input Image : ")
 outputPath = input("Enter Output Path for the Image : ")
 outputQuality = input("Enter Desired Quality of the Image (0-100) : ")
 
 print("IP: ", inputPath, "OP: ", outputPath, "OQ: ", outputQuality)
-
 
 compress_image(inputPath, outputPath, outputQuality)
 
