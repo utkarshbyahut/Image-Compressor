546914f950a393457087d28b191b96077f136ef3
utkarshbyahut <utkarshbyahut16@gmail.com>
2024-07-10 13:41:16 -0700

    Driver File

---
+++ main.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append("/opt/anaconda3/lib/python3.11/site-packages")
+
+from PIL import Image
+
+def compress_image(input_path, output_path, quality):
+    """
+    Compresses an image and saves it to the output path.
+
+    Args:
+        input_path: The path to the input image.
+        output_path: The path to save the compressed image.
+        quality: The compression quality (0-100).
+    """
+
+    img = Image.open(input_path)
+    img.save(output_path, optimize=True, quality=quality)
+
+inputPath = input("Enter Local Path to the Input Image : ")
+outputPath = input("Enter Output Path for the Image : ")
+outputQuality = input("Enter Desired Quality of the Image (0-100) : ")
+
+print("IP: ", inputPath, "OP: ", outputPath, "OQ: ", outputQuality)
+
+
+compress_image(inputPath, outputPath, outputQuality)
+
+
